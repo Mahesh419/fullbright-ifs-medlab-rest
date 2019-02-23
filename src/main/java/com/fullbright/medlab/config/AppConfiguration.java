ce4fcf61b1b5c7bdda33ad7ebfc00f3e2638cde8
Mahesh <maheshd95@gmail.com>
2019-02-23 13:01:59 +0530

    adding cross origin to endpoint(10)

--- src/main/java/com/fullbright/medlab/config/AppConfiguration.java
+++ src/main/java/com/fullbright/medlab/config/AppConfiguration.java
@@ -1,22 +1,43 @@
 package com.fullbright.medlab.config;
 
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
-public class AppConfiguration implements WebMvcConfigurer {
+public class AppConfiguration {
 	
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-        .allowedOrigins("*")
-            .allowCredentials(false);
-
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+ 
+ @Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+ public DispatcherServlet dispatcherServlet() {
+     DispatcherServlet dispatcherServlet = new DispatcherServlet();
+     dispatcherServlet.setDispatchOptionsRequest(true);
+     return dispatcherServlet;
+ }
+ 
 	
 }
