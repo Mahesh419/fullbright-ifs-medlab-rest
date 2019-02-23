43048c3271a73b4bc3f2a702a1dc8a6711757809
Mahesh <maheshd95@gmail.com>
2019-02-23 11:43:36 +0530

    adding cross origin to endpoint(5)

--- src/main/java/com/fullbright/medlab/config/AppConfiguration.java
+++ src/main/java/com/fullbright/medlab/config/AppConfiguration.java
@@ -13,9 +13,10 @@ public class AppConfiguration implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedOrigins("http://nawalokamedlabs.herokuapp.com")
+                .allowedOrigins("*")
                 .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
-            .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin");
+            .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
+            .allowCredentials(false);
 
     }
 	
